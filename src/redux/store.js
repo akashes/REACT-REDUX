468b63298aed3315d4aeaa8d969558d57d142b05
akash <akashes5753279@gmail.com>
2025-03-31 23:03:56 +0530

    feat: implement logger middleware to log prevstate dispatched action and new state whenever state changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { createStore } from "redux";
 import { rootReducer } from "./combineReducers";
+import { applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+const logger = createLogger()
 
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,applyMiddleware(logger))
